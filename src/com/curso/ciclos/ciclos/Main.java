c5bec329bf8085ec029efba2c0f18194f6276a36
Carlos85cba <52687494+Carlos85cba@users.noreply.github.com>
2019-07-21 01:36:47 -0300

    Se agrega menu en ciclos

--- src/com/curso/ciclos/ciclos/Main.java
+++ src/com/curso/ciclos/ciclos/Main.java
@@ -1,24 +1,51 @@
 package com.curso.ciclos.ciclos;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int contador = 0;
+        System.out.println("Ingrese 1 para calcular sumatoria de Números");
+        System.out.println("Ingrese 2 para calcular sumatoria de cuadrados de Números");
+        System.out.println("Ingrese 3 para calcular promedio");
+
+        Scanner myScan = new Scanner(System.in);
+        System.out.println("Ingrese número menor a 80");
+        int nro1 = myScan.nextInt();
+        while (nro1>80) {
+            System.out.println("Ingrese número menor a 80");
+            nro1 = myScan.nextInt();}
+        System.out.println("Ingrese otro número menor a 100, mayor que el primero pero con diferencia menor a 20");
+        int nro2 = myScan.nextInt();
+        while (nro2>100 || nro1>nro2 || nro1<(nro2-20)  ) {
+            System.out.println("Ingrese otro número menor a 100, mayor que el primero pero con diferencia menor a 20");
+            nro2 = myScan.nextInt();}
+        sumaNumero(nro1,nro2);
+        sumaCuadrado(nro1,nro2);
+
+
+
+
+    }
+
+    public static void  sumaNumero(int nro1,int nro2){
+        int fromNumber = nro1;
         int suma = 0;
-        while (contador <= 50) {
-            suma = suma + contador;
-            contador++;
-            System.out.println("Contador " + contador + "suma " + suma);
+        while (fromNumber <= nro2) {
+            suma = suma + fromNumber;
+            fromNumber++;
+            System.out.println("Contador " + fromNumber + "suma " + suma);
+    }
+}
+
+    public static void  sumaCuadrado(int nro1,int nro2){
+        int fromNumber = nro1;
+        int suma = 0;
+        do {
+            suma = suma + (fromNumber*fromNumber);
+            fromNumber++;
+            System.out.println("Contador " + fromNumber + "suma " + suma);
 
 
-        }
-        contador = 0;
-        suma = 0;
-        while (contador <= 400) {
-            suma = suma + contador;
-            contador++;
-            System.out.println("Contador " + contador + "suma " + suma);
-
-
-        }
+        }while (fromNumber <= nro2);
     }
 }
